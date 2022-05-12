379a03d343886de7497b0cf538de9d0bc5bf8d32
l050101 <l01062506156@gmail.com>
2022-05-12 12:28:33 +0900

    umm?

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import SassComponent from "./ScssComponent";
+import CSSModule from "./CSSModule";
+import StyledComponent1 from "./StyledComponent";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <SassComponent/> */}
+      {/* <CSSModule/> */}
+      <StyledComponent1/>
     </div>
   );
 }
